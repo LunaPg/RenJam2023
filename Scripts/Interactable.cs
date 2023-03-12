70dc0c7564ca6776ff4fd767ec3637e5ac939924
Audrey Paiement <audreyp@unity3d.com>
2023-03-12 17:13:08 -0400

    Made interactable generic

--- Scripts/Interactable.cs
+++ Scripts/Interactable.cs
@@ -1,8 +1,44 @@
-using System.Threading.Tasks;
 using Godot;
 
-public partial interface Interactable
+public partial class Interactable : Node2D
 {
+	[Export]
+	Resource DialogueResource;
+	[Export]
+	string DialogueTitle;
+	private Sprite2D _tooltip;
+	private bool _canInteract;
+	private bool _dialogOpened;
 
-    public Task Interact();
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_tooltip = GetNode<Sprite2D>("Tooltip");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (_canInteract && Input.IsActionPressed("interact") && !_dialogOpened) {
+			_dialogOpened = true;
+			GetNode<DialogueUiManager>("/root/DialogueUiManager").StartDialogue(DialogueResource, DialogueTitle);
+		}
+	}
+
+    private void OnDialogueFinished()
+    {
+        _dialogOpened = false;
+    }
+
+	public void OnInteractionAreaEntered(Node2D body) 
+	{
+		_canInteract = true;
+		_tooltip.Visible = true;
+	}
+
+	public void OnInteractionAreaExited(Node2D body) 
+	{
+		_tooltip.Visible = false;
+		_canInteract = false;
+	}
 }
